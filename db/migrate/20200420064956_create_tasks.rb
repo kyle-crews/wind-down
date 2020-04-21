f8ea5bc89935776ef2cb567c3183fb85d195be41
github username <github email address>
2020-04-21 02:09:29 -0500

    updated migrations

--- db/migrate/20200420064956_create_tasks.rb
+++ db/migrate/20200420064956_create_tasks.rb
@@ -1,4 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
+    create_table :tasks do |t|
+      t.string :entry
+    end
   end
 end
--- db/migrate/20200420065021_create_notes.rb
+++ db/migrate/20200420065021_create_notes.rb
@@ -1,4 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
+    create_table :notes do |t|
+      t.text :entry
+    end
   end
 end
