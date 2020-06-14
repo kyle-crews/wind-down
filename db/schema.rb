f7d25b2f84bf79d3573d5f69a914dcba3cec7d29
github username <github email address>
2020-06-14 02:13:12 -0500

    dropped development DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720175448) do
-
-  create_table "days", force: :cascade do |t|
-    t.string  "name"
-    t.integer "user_id"
-  end
-
-  create_table "logs", force: :cascade do |t|
-    t.string  "good"
-    t.string  "accomplishment"
-    t.string  "date"
-    t.integer "day_id"
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
